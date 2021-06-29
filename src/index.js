ce0137cf92bc1dac10b2bd331e043236cc34407b
ch2isk4kos <ch2isk4kos@gmail.com>
2021-06-29 00:05:02 -0400

    add root field of Link and resolver functions to fields

--- src/index.js
+++ src/index.js
@@ -1,20 +1,43 @@
 const { ApolloServer } = require("apollo-server");
 
+// root fields Query, Mutation, Subscription define the available API operations
+
 // 1: Defines GraphQL schema
 const typeDefs = `
   type Query {
     info: String!
+    feed: [Link!]!
+  }
+  type Link {
+    id: ID!
+    description: String!
+    url: String!
   }
 `;
 // '!' means that value cannot be null
+
+let links = [
+  {
+    id: "link-0",
+    url: "www.howtographql.com",
+    description: "Fullstack tutorial for GraphQL",
+  },
+];
 
 // 2: implementation of GraphQL schema
 const resolvers = {
   Query: {
     //   must return the same time as defined in typeDef
     info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links,
+  },
+  Link: {
+    id: (parent) => parent.id,
+    description: (parent) => parent.description,
+    url: (parent) => parent.url,
   },
 };
+// GraphQL queries resolve by invoking the resolver functions for the fields contained in the query.
 
 // 3: Schemas get bundled and passed to the server
 const server = new ApolloServer({
