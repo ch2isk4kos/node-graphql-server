6a6caeefc2da50e3c26bad02944546753934e792
ch2isk4kos <ch2isk4kos@gmail.com>
2021-07-03 16:26:23 -0400

    add/export new vote subscription

--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -11,8 +11,20 @@ const newLink = {
   },
 };
 
+function newVoteSubscribe(parent, args, context, info) {
+  return context.pubsub.asyncIterator("NEW_VOTE");
+}
+
+const newVote = {
+  subscribe: newVoteSubscribe,
+  resolve: (payload) => {
+    return payload;
+  },
+};
+
 module.exports = {
   newLink,
+  newVote,
 };
 
 // Resolvers for subscriptions are slightly different than the ones for queries and mutations:
